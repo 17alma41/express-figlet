903dba1aa36978c9720b6c737f7ff5a011c779f8
alvaro <alvarodevejer4@gmail.com>
2024-11-04 10:48:35 +0100

    ping funcionando

--- server.js
+++ server.js
@@ -2,9 +2,12 @@ const express = require('express')
 const { exec } = require('child_process')
 //const figlet = require('figlet')
 
-const comando = 'echo "hola mundo" > hola.txt'
+//const comando = 'echo "hola mundo" > hola.txt'
+
 const app = express()
 const port = 8000
+
+app.use(express.static("public"))
 
 app.get("/", (req, res) => {
     exec(comando, (error, stdout, stderr) =>{
@@ -12,6 +15,17 @@ app.get("/", (req, res) => {
     });
 })
 
+app.get("/ping", (req, res) => {
+    const dominio = req.query.dominio
+    const comandoPing = `ping -c 4 ${dominio}`
+
+    exec(comandoPing, (error, stdout, stderr) =>{
+        res.send(stdout)
+    });
+})
+
+
+
 app.listen(port, () => {
-    console.log("Servidor iniciado")
+    console.log(`Servidor iniciado en http://localhost:${port}`)
 })
